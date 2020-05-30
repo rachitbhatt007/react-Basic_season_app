c9fbb04b8013ece10217b2a7490bd77ecf216b74
Rachit Bhatt <rachit.bhatt07@gmail.com>
2020-05-30 20:13:02 +0530

    season display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader'
+
+
+class App extends Component{
+	state={lat:null,errorMessage:null};
+
+		
+
+	
+
+
+	componentDidMount()
+	{
+		window.navigator.geolocation.getCurrentPosition(
+		position=>this.setState({lat: position.coords.latitude }),
+		(err)=>this.setState({errorMessage:err.message})
+		);
+
+	}
+
+
+
+
+	render()
+	{
+	if(this.state.errorMessage && !this.state.lat){
+		return <div>Error:{this.state.errorMessage}</div>
+	}
+	if(!this.state.errorMessage && this.state.lat){
+		return<SeasonDisplay lat={this.state.lat}/>
+	}
+	return <Loader/>
+}	
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+<App/>,document.querySelector('#root'))
